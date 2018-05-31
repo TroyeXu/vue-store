9fc02bce1651ad42d474eec67932944dab7f8608
Troyexu <zooka8548@gmail.com>
2018-05-31 19:30:54 +0800

    abc

---
+++ all.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.man').animate({
+        opacity: 0.25,
+        height: "toggle"
+    }, 3000)
+    
+});
---
+++ data.js
@@ -0,0 +1,88 @@
+const baseData = [{
+    "id": 1,
+    "price": "100以內",
+    "distance": "騎車10分以上",
+    "delivery": "X",
+    "type": ["飯", "麵", "異國料理"],
+    "hours": ["中", "晚"]
+},
+{
+    "id": 2,
+    "price": "200以內",
+    "distance": "騎車10分鐘",
+    "delivery": "X",
+    "type": ["麵", "異國料理"],
+    "hours": ["中", "晚"]
+},
+{
+    "id": 3,
+    "price": "100以內",
+    "distance": "騎車10分以上",
+    "delivery": "O",
+    "type": ["飲料"],
+    "hours": ["中", "晚"]
+},
+{
+    "id": 4,
+    "price": "100以內",
+    "distance": "騎車10分鐘",
+    "delivery": "X",
+    "type": ["飲料", "甜點咖啡", "異國料理"],
+    "hours": ["早", "中"]
+},
+{
+    "id": 5,
+    "price": "200以內",
+    "distance": "騎車10分以上",
+    "delivery": "O",
+    "type": ["飯", "麵"],
+    "hours": ["中", "晚"]
+},
+{
+    "id": 6,
+    "price": "100以內",
+    "distance": "校內",
+    "delivery": "X",
+    "type": ["麵", "異國料理"],
+    "hours": "早"
+},
+{
+    "id": 7,
+    "price": "100以內",
+    "distance": "騎車10分鐘",
+    "delivery": "O",
+    "type": ["飯", "麵", "異國料理", "便當"],
+    "hours": ["中", "晚"]
+},
+{
+    "id": 8,
+    "price": "200~300以內",
+    "distance": "騎車10分鐘",
+    "delivery": "X",
+    "type": [
+        "飯", "異國料理"
+    ],
+    "hours": ["中", "晚"]
+},
+{
+    "id": 9,
+    "price": "200~300以內",
+    "distance": "騎車10分鐘",
+    "delivery": "O",
+    "type": ["飯", "麵"],
+    "hours": ["中", "晚"]
+},
+{
+    "id": 10,
+    "price": "300以上",
+    "distance": "校內",
+    "delivery": "O",
+    "type": ["便當"],
+    "hours": [
+        "中",
+        "晚"
+    ]
+}
+];
+
+export default baseData;
